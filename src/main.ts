c994e9671c14737a47f62b4a8642d5eb4f20c4e9
John Page <john@thejohnpage.com>
2026-09-18 18:36:27 +0000

    feat: bootstrap NestJS auth API

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  app.enableCors({ origin: ['http://localhost:4200', 'http://localhost:4201'], credentials: true });
+  await app.listen(process.env.PORT ?? 3001);
+}
+void bootstrap();
