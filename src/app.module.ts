78b0446d51ad12e37960e784318b2b5e8a76fd0f
John Page <john@thejohnpage.com>
2026-09-24 11:03:13 -0500

    Register auth health endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,14 @@ import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
 import { DatabaseService } from './database/database.service';
+import { HealthController } from './health.controller';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     JwtModule.register({ global: true }),
   ],
-  controllers: [AuthController],
+  controllers: [AuthController, HealthController],
   providers: [DatabaseService, AuthService],
 })
 export class AppModule {}
